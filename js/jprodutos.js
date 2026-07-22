beff825302f5d6776589b8a6ec204ec0865d1704
Rose Galindo <158229168+Rosegalindo@users.noreply.github.com>
2026-07-22 15:24:49 -0300

    Organizando a estrutura
    Organizando a estrutura e inserindo mais setores

---
+++ js/jprodutos.js
@@ -0,0 +1,698 @@
+const produtos = {
+   
+// ==========================
+// FEMININO
+// ==========================
+    {
+    id: 1,
+    categoria: "feminino",
+    nome: "Zaya",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 1.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 2,
+    categoria: "feminino",
+    nome: "Al Sabah",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 2.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 3,
+    categoria: "feminino",
+    nome: "Hasan Rose",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 3.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 4,
+    categoria: "feminino",
+    nome: "D",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 4.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 5,
+    categoria: "feminino",
+    nome: "GD Légère",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 5.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 6,
+    categoria: "feminino",
+    nome: "Mystère",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 6.png",
+    familia: "Floral Amadeirado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 7,
+    categoria: "feminino",
+    nome: "Change",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 7.png",
+    familia: "Chipre Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 8,
+    categoria: "feminino",
+    nome: "Liberté",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 8.png",
+    familia: "Fougère Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 9,
+    categoria: "feminino",
+    nome: "Famous for Her",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 9.png",
+    familia: "Floral Amadeirado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 10,
+    categoria: "feminino",
+    nome: "521 Young for Her",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 10.png",
+    familia: "Floral Frutado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 11,
+    categoria: "feminino",
+    nome: "Intense Girl",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 11.png",
+    familia: "Floral Frutado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 12,
+    categoria: "feminino",
+    nome: "Classic",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 51.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 13,
+    categoria: "feminino",
+    nome: "Love",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 12.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 14,
+    categoria: "feminino",
+    nome: "Anjos",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 13.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 15,
+    categoria: "feminino",
+    nome: "Clássica",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 14.png",
+    familia: "Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 16,
+    categoria: "feminino",
+    nome: "Emoções",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 15.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 17,
+    categoria: "feminino",
+    nome: "Queen",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 16.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 18,
+    categoria: "feminino",
+    nome: "Summer Flowers",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 17.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 19,
+    categoria: "feminino",
+    nome: "Voluptuous",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 18.png",
+    familia: "Chipre Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 20,
+    categoria: "feminino",
+    nome: "Be You",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 19.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 21,
+    categoria: "feminino",
+    nome: "Escandalosa",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 20.png",
+    familia: "Chipre Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 22,
+    categoria: "feminino",
+    nome: "Euphoric",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 21.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 23,
+    categoria: "feminino",
+    nome: "Green Apple",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 22.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 24,
+    categoria: "feminino",
+    nome: "Joy",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 23.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 25,
+    categoria: "feminino",
+    nome: "World",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 24.png",
+    familia: "Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 26,
+    categoria: "feminino",
+    nome: "Happiness",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 25.png",
+    familia: "Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 27,
+    categoria: "feminino",
+    nome: "All Sexes",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 52.png",
+    familia: "Floral Amadeirado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 28,
+    categoria: "feminino",
+    nome: "Felina",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 26.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 29,
+    categoria: "feminino",
+    nome: "Delice",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 56.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 30,
+    categoria: "feminino",
+    nome: "Alen",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 27.png",
+    familia: "Ambarado Amadeirado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 31,
+    categoria: "feminino",
+    nome: "Attractive",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 53.png",
+    familia: "Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 32,
+    categoria: "feminino",
+    nome: "Bouquet",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 28.png",
+    familia: "Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 33,
+    categoria: "feminino",
+    nome: "Miss",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 54.png",
+    familia: "Chipre Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 34,
+    categoria: "feminino",
+    nome: "Wonderful Girl",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 55.png",
+    familia: "Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 35,
+    categoria: "feminino",
+    nome: "Águas Marinhas",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 29.png",
+    familia: "Floral Leve Aquático",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 36,
+    categoria: "feminino",
+    nome: "Night´s Lady",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 57.png",
+    familia: "Floral Amadeirado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 37,
+    categoria: "feminino",
+    nome: "Clô",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 30.png",
+    familia: "Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 38,
+    categoria: "feminino",
+    nome: "Chic Woman",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 31.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 39,
+    categoria: "feminino",
+    nome: "Angelina",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 33.png",
+    familia: "Ambarado Gourmand",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 40,
+    categoria: "feminino",
+    nome: "L'Eternite",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 32.png",
+    familia: "Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 41,
+    categoria: "feminino",
+    nome: "Nº5",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 34.png",
+    familia: "Floral Aldéidico",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 42,
+    categoria: "feminino",
+    nome: "521 Vip Rosé",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 35.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 43,
+    categoria: "feminino",
+    nome: "AK Woman",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 42.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 44,
+    categoria: "feminino",
+    nome: "Hypnotize",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 36.png",
+    familia: "Ambarado Gourmand",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 45,
+    categoria: "feminino",
+    nome: "Elegance",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 37.png",
+    familia: "Floral Aldéidico",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 46,
+    categoria: "feminino",
+    nome: "Exclusive Code",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 38.png",
+    familia: "Floral",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 47,
+    categoria: "feminino",
+    nome: "521 Woman",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 39.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 48,
+    categoria: "feminino",
+    nome: "521 Vip",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 40.png",
+    familia: "Floral Gourmand",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 49,
+    categoria: "feminino",
+    nome: "521 Sexy",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 41.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 50,
+    categoria: "feminino",
+    nome: "Elegance Light Blue",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 43.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 51,
+    categoria: "feminino",
+    nome: "Gabriela",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 44.png",
+    familia: "Floral Amadeirado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 52,
+    categoria: "feminino",
+    nome: "Je t'aime ",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 45.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 53,
+    categoria: "feminino",
+    nome: "Lady Money",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 46.png",
+    familia: "Floral Frutal",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 54,
+    categoria: "feminino",
+    nome: "GD",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 47.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 55,
+    categoria: "feminino",
+    nome: "Luxuria",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 48.png",
+    familia: "Ambarado Gourmand",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 56,
+    categoria: "feminino",
+    nome: "Fantastic",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 49.png",
+    familia: "Floral Gourmand",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+{
+    id: 57,
+    categoria: "feminino",
+    nome: "Athena",
+    preco: 43.90,
+    imagem: "../img/feminino/Feminino 50.png",
+    familia: "Floral Ambarado",
+    volume: "15ml",
+    estoque: 50,
+    ativo: true
+},
+
+// ==========================
+// MASCULINO
+// ==========================
+
+
+
+
+
+};
