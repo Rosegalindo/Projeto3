6cac9b2c81cada150d05894efdb8135a2946f287
Rose Galindo <158229168+Rosegalindo@users.noreply.github.com>
2026-09-11 12:18:51 -0300

    Atualizando arquitetura
    Inserindo linhas de código da APIs

--- js/pagamento.js
+++ js/pagamento.js
@@ -98,13 +98,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const apiUrl =
                 window.API_URL ||
-                "http://localhost:3000";
-
-            const url = apiUrl + "/criar-preferencia";
-
-
-            console.log("🚀 ENVIANDO PEDIDO PARA O BACKEND...");
-            console.log("🌐 URL:", url);
+               (
+                window.location.hostname === "localhost" ||
+                window.location.hostname === "127.0.0.1"
+            ? "http://localhost:3000"
+            : "https://api.solverstore.com.br");
 
 
             // --------------------------------------------------
